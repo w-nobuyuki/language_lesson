f351b44ec66b176d07bb8afa1df628aac4e20ce3
vagrant <vagrant@localhost.localdomain>
2020-06-17 21:01:57 +0900

    各利用者のログアウト機能を追加

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,18 @@
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!
+
+  protected
+
+  def after_sign_out_path_for(resource)
+    case resource
+    when :user
+      new_user_session_path
+    when :teacher
+      new_teacher_session_path
+    when :admin
+      new_admin_session_path
+    else
+      new_user_session_path
+    end
+  end
 end
