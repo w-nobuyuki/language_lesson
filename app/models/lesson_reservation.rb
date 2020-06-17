7722b35350d7ebc44a4beab33d6f69348840627c
vagrant <vagrant@localhost.localdomain>
2020-06-18 00:44:10 +0900

    他のレッスンがすでに入っている日時は予約できないように修正

--- app/models/lesson_reservation.rb
+++ app/models/lesson_reservation.rb
@@ -3,4 +3,14 @@ class LessonReservation < ApplicationRecord
   belongs_to :user
 
   validates :zoom_url, presence: true
+  validate :cannot_reserve_same_datetime
+
+  def cannot_reserve_same_datetime
+    LessonReservation.where(user_id: user_id).each do |reservation|
+      next if reservation.lesson.start_at != lesson.start_at
+
+      errors.add(:base, 'その日時は他のレッスンを予約済みです')
+      break
+    end
+  end
 end
