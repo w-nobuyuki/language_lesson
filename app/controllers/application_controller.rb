b177693758565243a2b6e6e1a1878579efa1e3fc
vagrant <vagrant@localhost.localdomain>
2020-06-18 08:36:51 +0900

    ユーザー情報に名前を追加（途中）

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -15,4 +15,9 @@ class ApplicationController < ActionController::Base
       new_user_session_path
     end
   end
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit :sign_up, keys: %i[name]
+    devise_parameter_sanitizer.permit :account_update, keys: %i[name]
+  end
 end
