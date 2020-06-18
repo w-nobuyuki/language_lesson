da60bd00ff458865e0f15fe15ad1e48da8807109
w.nobuyuki <danboard1114@gmail.com>
2020-06-18 10:18:56 +0900

    ユーザー名の表示機能を追加

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,6 @@
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!
+  before_action :configure_permitted_parameters, if: :devise_controller?
 
   protected
 
@@ -18,6 +19,5 @@ class ApplicationController < ActionController::Base
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit :sign_up, keys: %i[name]
-    devise_parameter_sanitizer.permit :account_update, keys: %i[name]
   end
 end
