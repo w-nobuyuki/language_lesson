4b5f71bf9b44087d2e4be32fae0f2314c09d5450
vagrant <vagrant@localhost.localdomain>
2020-06-19 18:43:29 +0900

    レッスン予約一覧をbootstrap化、レッスン予約時の処理のリファクタリング

--- app/controllers/lesson_reservations_controller.rb
+++ app/controllers/lesson_reservations_controller.rb
@@ -9,15 +9,15 @@ class LessonReservationsController < ApplicationController
       return
     end
     @lesson_reservation = current_user.lesson_reservations.new_with_zoom_url(lesson_reservation_params)
-    current_user.lesson_tickets.first.destroy! if @lesson_reservation.valid?
 
     respond_to do |format|
       if @lesson_reservation.save
+        current_user.lesson_tickets.first.destroy!
         LessonMailer.notice_user(@lesson_reservation.lesson).deliver_now
         LessonMailer.notice_teacher(@lesson_reservation.lesson).deliver_now
         format.html { redirect_to lessons_path, notice: 'レッスンを予約しました。' }
       else
-        format.html { redirect_to lessons_path, alert: 'レッスンの予約に失敗しました。' }
+        format.html { redirect_to lessons_path, alert: @lesson_reservation.errors.full_messages.join }
       end
     end
   end
