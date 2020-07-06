ef44b95d5633fb4413d910df19aa799cc2526679
vagrant <vagrant@localhost.localdomain>
2020-07-06 19:26:58 +0900

    不要なassociationを削除

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,6 @@ class User < ApplicationRecord
   has_many :charges, dependent: :destroy
   has_many :lesson_tickets, dependent: :destroy
   has_many :lesson_reservations, through: :lesson_tickets
-  has_many :notifications, through: :lesson_reservations
 
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
